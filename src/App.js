f2dd8b69663aa053b9232e2316ed9a3c06c004ad
Diganta Das <ddas461598@gmail.com>
2020-10-10 16:24:43 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Home from './Home';
 import Main from './Main';
+import About from './pages/About';
+import Github from './pages/Github'
 import { AuthProvider } from './Auth';
 import PrivateRoute from './PrivateRoute';
 import './App.scss'
@@ -16,6 +18,8 @@ function App() {
           <div>
             <PrivateRoute exact path="/" component={Home} />
             <Route exact path="/main" component={Main} />
+            <Route exact path="/pages/about" component={About} />
+            <Route exact path="/pages/github" component={Github} />
           </div>
         </Router>
       </AuthProvider>
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 import React, { useState, useCallback, useContext } from 'react';
-import { withRouter, Redirect ,Link} from 'react-router-dom';
+import { withRouter, Redirect, Link } from 'react-router-dom';
 import firebaseConfig from './firebase'
 import { AuthContext } from './Auth';
 import { Button } from '@material-ui/core';
+import { BsMoon, BsSun} from 'react-icons/bs'
 
 
 
@@ -12,17 +13,23 @@ import { Button } from '@material-ui/core';
 
 const Main = ({ history }) => {
     const [showNav, setShowNav] = useState(false);
-    const [openLogIn, setOpenLogIn] = useState(false)
+    const [openLogIn, setOpenLogIn] = useState(false);
+    const [loginText, setLoginText] = useState('LOG IN');
+    const [nightMode, setNightMode] = useState(false);
 
     //Modal
-    const handelOpen = () =>{
+    const handelOpen = () => {
         setOpenLogIn(!openLogIn);
-        
+        setLoginText(openLogIn?"LOG IN":"CANCEL");
+
     }
 
-    
+    const nightModeToggle = () => {
+        setNightMode(!nightMode)
+    }
 
-    
+
+
 
 
     const handleLogIn = useCallback(async event => {
@@ -48,13 +55,17 @@ const Main = ({ history }) => {
     return (
 
         <div className="">
-            <div className="header">
-                <div className="hamburger" onClick={() => { setShowNav(!showNav) }} >
+            <div className={nightMode?"headerDark":"header"}>
+                <div className={nightMode ? "hamburgerDark" : "hamburger"} onClick={() => { setShowNav(!showNav); setOpenLogIn(!setOpenLogIn); setLoginText("LOG IN") }} >
                     <div className="div"></div>
                     <div className="div"></div>
                     <div className="div"></div>
                 </div>
-                <h1 className="headerText">Katana</h1>
+                <div className="headerText"><h1>Katana</h1></div>
+                
+                <div className="modes" onClick={nightModeToggle}>
+                    {nightMode ? (<div style={{ color: 'white', backgroundColor:'black' }}><BsSun size={32} /></div>) : (<div><BsMoon size={32} /></div>)}
+                </div>
             </div>
 
             <div className={showNav ? "navBar" : "hidden"}>
@@ -65,26 +76,29 @@ const Main = ({ history }) => {
                 </div>
                 <div className="line"></div>
                 <div className="loginButton">
-                    <Button  variant="contained" color="primary" onClick={handelOpen}>
-                        Log In
+                    <Button variant="contained" color="primary" onClick={handelOpen}>
+                        {loginText}
                     </Button>
                 </div>
-                <div className={openLogIn ? "loginForm":"hidden"}>
-                <h1 className="Login_Header">Log In</h1>
-                <form className="loginFields" onSubmit={handleLogIn}>
+                <div className={openLogIn ? "loginForm" : "hidden"}>
+                    <h1 className="Login_Header">Log In</h1>
+                    <form className="loginFields" onSubmit={handleLogIn}>
                         <label htmlFor="">Email </label>
-                <input type="email" name="email" placeholder="Email" />
-                    
-                        <label htmlFor="">Password </label>
-                <input type="password" name="password" placeholder="Password" />
-                    
-                    <button className="loginButton" type="submit">Log In</button>
-                </form>
-            </div>
+                        <input type="email" name="email" placeholder="Email" />
 
-               <div className="about">
-                    <Link to="pages/About" >About</Link>
-               </div>
+                        <label htmlFor="">Password </label>
+                        <input type="password" name="password" placeholder="Password" />
+
+                        <button className="loginButton" type="submit">Log In</button>
+                    </form>
+                </div>
+
+                <div className="about">
+                    <Link to="/pages/about" >About</Link>
+                </div>
+                <div className="github">
+                    <Link to="/pages/github" >GitHub</Link>
+                </div>
             </div>
         </div>
     )
